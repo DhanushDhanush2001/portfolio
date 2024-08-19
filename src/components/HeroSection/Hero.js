8544612822f50571e8b25926528964674aa83f9d
DhanushDhanush2001 <dhanushpandian.0207@gmail.com>
2024-08-19 21:07:20 +0530

    latest commit

--- src/components/HeroSection/Hero.js
+++ src/components/HeroSection/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HeroContainer, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import { HeroContainer, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
 import HeroImg from '../../Images/HeroImage.png'
 import Typewriter from 'typewriter-effect';
 import { Bio } from "../../data/constants"
