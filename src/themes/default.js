c36d88925e4206bd045b4a32131a2dd5d4d2c9ce
DhanushDhanush2001 <dhanushpandian.0207@gmail.com>
2024-08-19 16:40:43 +0530

    initial commit

---
+++ src/themes/default.js
@@ -0,0 +1,27 @@
+export default{
+      // Temp fonts
+    fonts: {
+        title: "Poppins, sans-serif",
+        main: "Poppins, sans-serif",
+       },
+
+       //colors
+       colors:{
+          primary : "#854CE6",
+          background: "#222A35",
+          button: "#854CE6",
+          background2: "#19212C",
+          text: "C8CFD8",
+          text1: "F2F5F7",
+          text2: "#626970",
+          text3: "#575C66",
+          footerBackground: "#00012B",
+         },
+         // Breakpoints for responsive design
+         breakpoints:{
+            sm: 'screen and (maxwidth: 640px)',
+            md: 'screen and (maxwidth: 768px)',
+            lg: 'screen and (maxwidth: 1024px)',
+            xl: 'screen and (maxwidth: 1280px)',
+         },
+}
